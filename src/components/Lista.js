4aadeaf4ee9c00b1ca73f9fc30416c9cf78209b2
SoimSei <imsergio@hotmail.es>
2021-02-10 20:38:13 -0300

    login

---
+++ src/components/Lista.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+import ListaEmpresa from "./ListaEmpresa";
+
+const Lista = () => {
+  return (
+    <div className="lista">
+      <h2>Empresas</h2>
+      <div className="lista-empresas">
+        <ListaEmpresa
+          nombreEmpresa={"Empresa1"}
+          descripcionEmpresa={"Desc empresa 1"}
+        />
+        <ListaEmpresa
+          nombreEmpresa={"Empresa2"}
+          descripcionEmpresa={"Desc empresa 2"}
+        />
+        <ListaEmpresa
+          nombreEmpresa={"Empresa3"}
+          descripcionEmpresa={"Desc empresa 3"}
+        />
+        <ListaEmpresa
+          nombreEmpresa={"Empresa3"}
+          descripcionEmpresa={"Desc empresa 3"}
+        />
+        <ListaEmpresa
+          nombreEmpresa={"Empresa3"}
+          descripcionEmpresa={"Desc empresa 3"}
+        />
+        <ListaEmpresa
+          nombreEmpresa={"Empresa3"}
+          descripcionEmpresa={"Desc empresa 3"}
+        />
+        <ListaEmpresa
+          nombreEmpresa={"Empresa3"}
+          descripcionEmpresa={"Desc empresa 3"}
+        />
+        <ListaEmpresa
+          nombreEmpresa={"Empresa3"}
+          descripcionEmpresa={"Desc empresa 3"}
+        />
+        <ListaEmpresa
+          nombreEmpresa={"Empresa3"}
+          descripcionEmpresa={"Desc empresa 3"}
+        />
+        <ListaEmpresa
+          nombreEmpresa={"Empresa3"}
+          descripcionEmpresa={"Desc empresa 3"}
+        />
+        <ListaEmpresa
+          nombreEmpresa={"Empresa3"}
+          descripcionEmpresa={"Desc empresa 3"}
+        />
+      </div>
+    </div>
+  );
+};
+export default Lista;
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav>
+      <h1>Temp name</h1>
+    </nav>
+  );
+};
+
+export default Nav;
